0e8fa1ecfacff5166522d415bc5bc55cfb31b123
Gaurav Dixit <gaurav.dixit01@nagarro.com>
2019-04-17 13:47:51 +0530

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>\n  <p>Demo App test </p>');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
